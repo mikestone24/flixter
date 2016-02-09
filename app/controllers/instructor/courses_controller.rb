547acc86835cdbd5e0bd2d439a577d5f0cb9da1d
Mike Stone <michaeljacobstone@gmail.com>
2016-02-08 20:27:45 -0800

    chapter 4 complete
    Added alot of features. Accidentally ran git init .  then deleted .git
    folder after reading some instructions but they assumed there was no
    commits made.

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,16 +1,13 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def new
     @course = Course.new
   end
 
-  def show
-    @course = Course.find(params[:id])
-  end
-
   def create
-     @course = current_user.courses.create(course_params)
+    @course = current_user.courses.create(course_params)
     if @course.valid?
       redirect_to instructor_course_path(@course)
     else
@@ -18,10 +15,22 @@ class Instructor::CoursesController < ApplicationController
     end
   end
 
-  private
-
-  def course_params
-    params.require(:course).permit(:title, :description, :cost)
+  def show
   end
 
+  private
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render text: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description, :cost,:image )
+  end
 end
