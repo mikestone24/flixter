e415494e064bf46f3fdf2f19aa2bebba6c6d5a97
Mike Stone <michaeljacobstone@gmail.com>
2016-02-09 22:19:41 +0000

    next lesson button

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,5 +3,10 @@ class Section < ActiveRecord::Base
   has_many :lessons
 
   include RankedModel
-  ranks :row_order, :with_same => :course_id	
+  ranks :row_order, :with_same => :course_id
+
+  def next_section
+    course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+  end
+
 end
